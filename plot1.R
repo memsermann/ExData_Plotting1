96761c93219fbea329bf291ec0cde41015d21754
memsermann <markus.emsermann@comcast.net>
2015-03-08 12:52:15 -0600

    Second Commit

--- plot1.R
+++ plot1.R
@@ -1,11 +1,9 @@
-
-
 #read in data
 power <- read.table("household_power_consumption.txt",header= TRUE,sep = ";",colClasses=c("character","character", "numeric","numeric","numeric","numeric", "numeric","numeric","numeric"), na.strings=c("NA", "?"))
 power<-subset(power, Date == "1/2/2007" | Date == "2/2/2007")
 power<-transform(power,Date = as.Date(Date,"%d/%m/%Y"), Time = strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"))
 
-
+#open plotting device
 png("plot1.png", width = 480, height = 480)
 
 #plot1
--- plot4.R
+++ plot4.R
@@ -1,15 +1,15 @@
-
 #read in data
 power <- read.table("household_power_consumption.txt",header= TRUE,sep = ";",colClasses=c("character","character", "numeric","numeric","numeric","numeric", "numeric","numeric","numeric"), na.strings=c("NA", "?"))
 power<-subset(power, Date == "1/2/2007" | Date == "2/2/2007")
 power<-transform(power,Date = as.Date(Date,"%d/%m/%Y"), Time = strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"))
 
+#open plotting device
 png("plot4.png", width = 480, height = 480)
 
 #plot4
 par(mfrow=c(2,2))
 
-plot(power$Time,power$Global_active_power, type="n",xlab = " ",ylab="Global Active Power (kilowatts)")
+plot(power$Time,power$Global_active_power, type="n",xlab = " ",ylab="Global Active Power")
 lines(power$Time,power$Global_active_power)
 
 plot(power$Time,power$Voltage, type="n",xlab = "datetime",ylab="Voltage")
